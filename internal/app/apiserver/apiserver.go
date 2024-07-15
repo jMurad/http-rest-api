0bf228dcaaeb9ff49d0460002b3d2b2deb765968
JMurad <jml.murad@gmail.com>
2024-07-15 15:40:18 +0300

    git init

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,83 @@
+package apiserver
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jMurad/http-rest-api/internal/app/store"
+	"github.com/sirupsen/logrus"
+)
+
+// APIServer ...
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+	store  *store.Store
+}
+
+// New ...
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+// Start ...
+func (s *APIServer) Start() error {
+	err := s.configureLogger()
+	if err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server")
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+// configureLogger ...
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
+
+// configureRouter ...
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+
+}
+
+// handleHello ...
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, fmt.Sprintf("Hello -> %v", r.Host))
+	}
+}
+
+// configureStore ...
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
+	return nil
+}
---
+++ internal/app/store/config.go
@@ -0,0 +1,10 @@
+package store
+
+type Config struct {
+	DatabaseURL string `toml:"database_url"`
+}
+
+// NewConfig ...
+func NewConfig() *Config {
+	return &Config{}
+}
