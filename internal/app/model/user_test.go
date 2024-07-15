09c8e4aed5ae7b0d7d9b3ee908415015fa6c6851
JMurad <jml.murad@gmail.com>
2024-07-15 22:44:56 +0300

    add validation

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -13,3 +13,80 @@ func TestUser_BeforeCreate(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotZero(t, u.EncryptedPassword)
 }
+
+func TestUser_Validate(t *testing.T) {
+	testCases := []struct {
+		name    string
+		u       func() *model.User
+		isValid bool
+	}{
+		{
+			name: "valid",
+			u: func() *model.User {
+				return model.TestUser(t)
+			},
+			isValid: true,
+		},
+		{
+			name: "empty email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = ""
+
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "invalid email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = "invalid"
+
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "empty password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "short password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = "short"
+
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "encrypted password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				u.EncryptedPassword = "encryptedpassword"
+
+				return u
+			},
+			isValid: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.u().Validate())
+			} else {
+				assert.Error(t, tc.u().Validate())
+			}
+		})
+	}
+}
