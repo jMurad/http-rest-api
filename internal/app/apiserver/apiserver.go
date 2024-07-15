a4a52bcf69b6bff9b8b70e7f0310773911854f24
JMurad <jml.murad@gmail.com>
2024-07-16 01:52:00 +0300

    refactoring

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,82 +1,35 @@
 package apiserver
 
 import (
-	"io"
+	"database/sql"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/jMurad/http-rest-api/internal/app/store"
-	"github.com/sirupsen/logrus"
+	"github.com/jMurad/http-rest-api/internal/app/store/sqlstore"
 )
 
-// APIServer ...
-type APIServer struct {
-	config *Config
-	logger *logrus.Logger
-	router *mux.Router
-	store  *store.Store
-}
-
-// New ...
-func New(config *Config) *APIServer {
-	return &APIServer{
-		config: config,
-		logger: logrus.New(),
-		router: mux.NewRouter(),
-	}
-}
-
 // Start ...
-func (s *APIServer) Start() error {
-	err := s.configureLogger()
+func Start(config *Config) error {
+	db, err := newDB(config.DataBaseURL)
 	if err != nil {
 		return err
 	}
+	defer db.Close()
 
-	s.configureRouter()
+	store := sqlstore.New(db)
+	srv := newServer(store)
 
-	if err := s.configureStore(); err != nil {
-		return err
-	}
-
-	s.logger.Info("starting api server")
-
-	return http.ListenAndServe(s.config.BindAddr, s.router)
+	return http.ListenAndServe(config.BindAddr, srv)
 }
 
-// configureLogger ...
-func (s *APIServer) configureLogger() error {
-	level, err := logrus.ParseLevel(s.config.LogLevel)
+func newDB(databaseURL string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", databaseURL)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	s.logger.SetLevel(level)
-
-	return nil
-}
-
-// configureRouter ...
-func (s *APIServer) configureRouter() {
-	s.router.HandleFunc("/hello", s.handleHello())
-
-}
-
-// handleHello ...
-func (s *APIServer) handleHello() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello")
-	}
-}
-
-// configureStore ...
-func (s *APIServer) configureStore() error {
-	st := store.New(s.config.Store)
-	if err := st.Open(); err != nil {
-		return err
+	if err := db.Ping(); err != nil {
+		return nil, err
 	}
 
-	s.store = st
-
-	return nil
+	return db, nil
 }
---
+++ internal/app/store/teststore/userrepository.go
@@ -0,0 +1,37 @@
+package teststore
+
+import (
+	"github.com/jMurad/http-rest-api/internal/app/model"
+	"github.com/jMurad/http-rest-api/internal/app/store"
+)
+
+// UserRepository ...
+type UserRepository struct {
+	store *Store
+	users map[string]*model.User
+}
+
+// Create ...
+func (r *UserRepository) Create(u *model.User) error {
+	if err := u.Validate(); err != nil {
+		return err
+	}
+
+	if err := u.BeforeCreate(); err != nil {
+		return err
+	}
+
+	r.users[u.Email] = u
+	u.ID = len(r.users)
+
+	return nil
+}
+
+// FindByEmail ...
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	if u, ok := r.users[email]; ok {
+		return u, nil
+	} else {
+		return nil, store.ErrRecordNotFound
+	}
+}
